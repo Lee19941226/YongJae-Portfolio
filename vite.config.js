d5055129e3c9eabd6a435d00a11fee886ff6a1e7
yeah~ <skjb6996@naver.com>
2025-12-31 15:19:10 +0900

    commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/My-portfolio/",
+  base: "/YongJae-Portfolio/",
   build: {
     outDir: "docs",
     emptyOutDir: true, // 매우 중요! 이전 파일 완전히 삭제
